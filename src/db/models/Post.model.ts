fd48b70b142098a084322fa90edf70b8f3e19aba
Denis Onder <denis_onder@protonmail.com>
2020-03-26 13:29:49 +0100

    Post model

---
+++ src/db/models/Post.model.ts
@@ -0,0 +1,41 @@
+import { Schema } from "mongoose";
+import generator from "./model.generator";
+
+const schema = new Schema({
+  author: {
+    type: Schema.Types.ObjectId,
+    required: true
+  },
+  title: {
+    type: String,
+    required: true
+  },
+  handle: {
+    type: String,
+    required: true,
+    unique: true
+  },
+  body: {
+    type: String,
+    required: true
+  },
+  likes: [{ type: Schema.Types.ObjectId }],
+  comments: [
+    {
+      user: {
+        type: Schema.Types.ObjectId,
+        required: true
+      },
+      body: {
+        type: String,
+        required: true
+      }
+    }
+  ],
+  createdAt: {
+    type: Date,
+    default: Date.now
+  }
+});
+
+export default generator("user", schema);
