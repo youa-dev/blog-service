6e3967e4301d67f448cbf3046e58acf44f492db4
Denis Onder <denis_onder@protonmail.com>
2020-04-11 21:26:12 +0200

    Added CORS

--- src/middleware/index.ts
+++ src/middleware/index.ts
@@ -3,8 +3,10 @@ import helmet from "helmet";
 import { json, urlencoded } from "body-parser";
 import logger from "./logger";
 import router from "./router";
+import cors from "cors";
 
 export default (app: Application): void => {
+  app.use(cors());
   app.use(helmet());
   app.use(json());
   app.use(urlencoded({ extended: true }));
